c1df5da4deaa24c72784901d09bc188701ec6f74
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2017-10-05 14:10:28 +0100

    first commit files

---
+++ project.py
@@ -0,0 +1,32 @@
+# TAI color
+# sem cor = 0
+# com cor > 0
+def get_no_color():
+    return 0
+def no_color(c):
+    return c==0
+def color(c):
+    return c > 0
+
+#TAI pos
+#Tuplo (l, c)
+def make_pos(l, c):
+    return (l, c)
+def pos_l(pos):
+    return pos[0]
+def pos_c(pos):
+    return pos[1]
+
+#TAI group
+#Lista [(l,c),(l,c),(l,c),...]
+
+#TAI board
+
+#Auxiliar function to draw a board
+#draw_board(<board>)
+def draw_board(board):
+    print(board)
+
+#board_find_groups(<board>) -> [2 valores]
+
+#board_remove_group(<board>, <group>) -> [5 valores]
