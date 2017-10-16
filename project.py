da7ef2f4cfb3af70760dd0ef3739673a43fff549
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2017-10-16 18:16:48 +0100

    close #9

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-# PROJETO IA 2017/2018
+ # PROJETO IA 2017/2018
 # Goncalo Ribeiro 82303
 # Andre Mendonca 82304
 
@@ -77,6 +77,63 @@ def connected_colors(position, board):
 		nodes.add(position)
 	return list(nodes)
 
+def vertical_compaction(board):
+	count = 0
+
+	for column in range(board_c(board)):
+		for row in range(board_l(board)-1, -1, -1):
+			position = make_pos(row, column)
+			position_content = board_position_content(position, board)
+
+			if position_content == 0:
+				count += 1
+				for index in range (row-1, -1, -1):
+					index_position = make_pos(index, column)
+					index_position_content = board_position_content(index_position, board)
+
+					if index_position_content == 0:
+						count += 1
+					else:
+						board[index+count][column] = index_position_content
+						board[index][column] = 0
+				count = 0
+				break
+	return board
+
+def horizontal_compaction(board):
+	row_init = board_l(board)-1
+	column_init = board_c(board)-1
+	count = 1
+
+	column = 0
+
+	while column <= column_init:
+		position = make_pos(row_init, column)
+		posCont = board_position_content(position, board)
+
+		if posCont == 0:
+			i_column = column +1
+			while i_column <= column_init:
+				pos = make_pos(row_init, i_column)
+				posC = board_position_content(pos, board)
+
+				if posC != 0:
+					i_row = row_init
+					while i_row >= 0:
+						pos_aux = make_pos(i_row, i_column)
+						pos_auxC = board_position_content(pos_aux, board)
+
+						if pos_auxC != 0:
+							board[i_row][i_column-count] = pos_auxC
+							board[i_row][i_column] = 0
+						i_row -= 1
+				else:
+					count += 1
+				i_column += 1
+			count = 1
+		column += 1
+	return board
+
 # ------------------------------------ TIPOS ------------------------------------#
 
 # TAI color
@@ -106,28 +163,40 @@ def board_l(board):
 	return len(board)
 
 def board_position_content(pos, board):
-	line = pos_l(pos)
+	row = pos_l(pos)
 	column = pos_c(pos)
-	return board[line][column]
+	return board[row][column]
+#def board_column_content(row, column, board):
+
 
 #board_find_groups(<board>) -> [2 valores]
 def board_find_groups(board):
-	modBoard = copy.deepcopy(board)
 	groups = []
 	global already_checked
 	global nodes
 
-	for line in range(len(modBoard)):
-		for column in range(len(modBoard[line])):
-			currentPos = make_pos(line, column)
+	for row in range(len(board)):
+		for column in range(len(board[row])):
+			currentPos = make_pos(row, column)
 
-			if board_position_content(currentPos, modBoard) != 0: #ignore the zeros on board
+			if board_position_content(currentPos, board) != 0: #ignore the zeros on board
 				if currentPos not in already_checked:
-					groups.append(connected_colors(currentPos, modBoard))
+					groups.append(connected_colors(currentPos, board))
 					nodes = set() #reset nodes from last connected_values search
 	return groups
 
 #board_remove_group(<board>, <group>) -> [5 valores]
+def board_remove_group(board, group):
+	modBoard = copy.deepcopy(board)
+	for pos in group:
+		row = pos_l(pos)
+		column = pos_c(pos)
+		modBoard[row][column] = 0
+
+	#return modBoard
+	#return vertical_compaction(modBoard)
+	return horizontal_compaction(vertical_compaction(modBoard))
+
 
 # ------------------------------------ EXEMPLOS DE CHAMADAS ------------------------------------#
 
@@ -143,6 +212,31 @@ b3 = [[3,1,3,2],[1,1,1,3],[1,3,2,1],[1,1,3,3],[3,3,1,2],[2,2,2,2],[3,1,2,3],[2,3
 b4 = [[3,1,3,2],[1,1,1,3],[1,3,2,1],[1,1,3,3],[3,3,1,2],[2,2,2,2],[3,1,2,3],[2,3,2,3],[2,1,1,3],[2,3,1,2]]
 #Tabuleiro 10x4, 5 cores
 b5 = [[1,1,5,3],[5,3,5,3],[1,2,5,4],[5,2,1,4],[5,3,5,1],[5,3,4,4],[5,5,2,5],[1,1,3,1],[1,2,1,3],[3,3,5,5]]
+#tabuleiro 4x5 com 0s
+b6 = [[0,0,0,0,0],[0,2,3,3,0],[1,2,1,3,0],[2,2,2,2,0]]
 
-#print(board_find_groups(b0))
-#draw_board(b0)
+b01 = [[0, 0, 0, 2], [0, 0, 3, 3], [0, 0, 2, 1], [3, 3, 3, 3], [3, 3, 1, 2], [2, 2, 2, 2], [3, 1, 2, 3], [2, 3, 2, 3], [2, 1, 1, 3], [2, 3, 1, 2]]
+
+b11 = [[1,3,2,1,2,1,2,2,1,2,2,1,1,3,1],[1,3,3,2,1,2,2,2,3,1,2,1,2,3,1],[1,1,1,2,3,2,3,3,2,2,3,1,1,3,1],[1,2,2,2,3,3,3,3,1,2,1,2,1,3,2],
+[1,3,1,3,2,2,2,2,3,1,1,2,3,2,1],[1,1,2,2,2,1,1,3,2,1,2,3,1,3,1],[3,1,3,2,2,2,3,3,3,1,3,3,2,1,1],[3,2,1,2,1,3,1,2,1,2,3,1,1,3,3],
+[2,3,1,2,3,3,1,2,3,3,3,2,1,1,1],[2,2,1,1,2,1,2,2,1,1,3,2,2,2,2]]
+
+b13 = [[4,4,4,2],[4,4,4,3],[4,4,4,1],[4,4,4,4],[4,4,4,2],[4,4,4,4],[4,4,4,3],[4,4,4,3],[4,4,4,4],[4,4,4,2]]
+
+r9 = [[0, 0, 0, 2], [0, 0, 3, 3], [0, 0, 2, 1], [3, 3, 3, 3], [3, 3, 1, 2], [2, 2, 2, 2], [3, 1, 2, 3], [2, 3, 2, 3], [2, 1, 1, 3], [2, 3, 1, 2]]
+
+r11 = [[0, 0, 0, 1, 2, 1, 2, 2, 1, 2, 2, 1, 1, 3, 1], [0, 0, 2, 2, 1, 2, 2, 2, 3, 1, 2, 1, 2, 3, 1], [0, 0, 3, 2, 3, 2, 3, 3, 2, 2, 3, 1, 1, 3, 1], 
+[0, 3, 2, 2, 3, 3, 3, 3, 1, 2, 1, 2, 1, 3, 2], [0, 3, 1, 3, 2, 2, 2, 2, 3, 1, 1, 2, 3, 2, 1], [0, 2, 2, 2, 2, 1, 1, 3, 2, 1, 2, 3, 1, 3, 1], 
+[3, 3, 3, 2, 2, 2, 3, 3, 3, 1, 3, 3, 2, 1, 1], [3, 2, 1, 2, 1, 3, 1, 2, 1, 2, 3, 1, 1, 3, 3], [2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 2, 1, 1, 1], 
+[2, 2, 1, 1, 2, 1, 2, 2, 1, 1, 3, 2, 2, 2, 2]]
+
+#draw_board(b13)
+#print
+#print
+#draw_board(r9)
+#print(board_find_groups(t3))
+#draw_board(board_remove_group(b6, [(1, 1), (2, 1), (3, 0), (3, 1), (3, 2),(3,3)]))
+#draw_board(board_remove_group(b4, [(0, 1), (1, 0), (1, 1), (1, 2), (2, 0), (3, 0), (3, 1)]))
+#draw_board(board_remove_group(b11, [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (5, 1), (6, 1), (2, 1), (2, 2)]))
+#draw_board(board_remove_group(b13, [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (9, 1), (8, 1), (7, 1), (6, 1), (5, 1), (4, 1), (3, 1), (2, 1), (1, 1), (0, 1), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2), (9, 2), (8, 3), (5, 3), (3, 3)]))
+
